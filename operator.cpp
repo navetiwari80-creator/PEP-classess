421060a1bc942eec2cd5c9b67d51fc3bc5968464
navetiwari80-creator <navetiwari80@gmail.com>
2026-07-12 23:28:54 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,179 @@
+#include<iostream>
+using namespace std;
+
+// int main()
+// {
+//     int a = 10;
+//     int b = 20;
+
+//     cout << "sum = " << a+b <<endl;
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a = 10;
+//     int b = 20;
+//     int c = 3;
+
+//     cout << a+b << endl; //30
+//     cout << b-a << endl; //10
+//     cout << a*b << endl; //200
+//     cout << b/a << endl; //2
+//     cout << b/c << endl; //6
+//     cout << a%c << endl; //1
+//     cout << b%c << endl; //2
+    
+//     return 0;
+
+// }    
+
+// int main(){
+//     int a = 10;
+//     int b = 20;
+
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+
+//     cout << a << endl;
+//     cout << b << endl;
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a, b, largest;
+
+//     cout << "Enter two numbers: ";
+//     cin >> a >> b;
+
+//     largest = (a > b) ? a : b;
+
+//     cout << "Largest Number = " << largest;
+
+//     return 0;
+// }
+
+
+// int main(){
+//     int a;
+
+//     cout << "Enter the number: ";
+//     cin >> a;
+
+//     if(a%2 == 0){
+//         cout << "Even Numbers: ";
+//     }else{
+//         cout << "Odd Numbers: ";
+//     }
+//     return 0;
+
+// }
+
+// int main()
+// {
+//     int num;
+
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+//     (num % 2 == 0) ? cout << "Even" : cout << "Odd";
+
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     int age;
+
+//     cout << "Enter your age: ";
+//     cin >> age;
+
+//     (age >= 18) ? cout << "Eligible for Voting"
+//                 : cout << "Not Eligible for Voting";
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int num;
+
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+//     if (num > 0)
+//         cout << "Positive";
+//     else if (num < 0)
+//         cout << "Negative";
+//     else
+//         cout << "Zero";
+
+//     return 0;
+// }
+
+
+// int main(){
+//     int a, b, c;
+
+//     cout << "Enter the Number: ";
+//     cin >> a >> b >> c;
+
+//     if(a > b && a > c){
+//         cout << " large a = " << a ;
+//     }
+//     else if(b>a && b>c){
+//         cout << " large b = " << b ;
+//     }
+//     else{
+//         cout << " Large c = " << c ;
+//     }
+
+//     return 0;
+// }
+
+// int main(){
+//     int a = 10;
+
+//     cout << "Original value : " << a << endl;
+//     cout << "Pre increment value : " << ++a << endl;
+//     cout << "Pre increment value : " << ++a << endl;
+//     cout << "Pre increment value : " << ++a << endl;
+
+//     cout << "Post increment value : " << a++ << endl;
+//     cout << "Post increment value : " << a++ << endl;
+//     cout << "Post increment value : " << a++ << endl;
+    
+//     cout << "Pre decrement value : " << --a << endl;
+//     cout << "Pre decrement value : " << --a << endl;
+//     cout << "Pre decrement value : " << --a << endl;
+//     cout << "Pre decrement value : " << --a << endl;
+    
+//     cout << "Post decrement value : " << a-- << endl;
+//     cout << "Post decrement value : " << a-- << endl;
+//     cout << "Post decrement value : " << a-- << endl;
+//     cout << "Post decrement value : " << a-- << endl;
+    
+//     return 0;
+// }    
+
+// int main(){
+//     int a = 20;
+
+//     cout << (a += 10) << endl;
+//     cout << (a -= 5) << endl;
+//     cout << (a *= 2) << endl;
+//     cout << (a /= 5) << endl;
+//     cout << (a %= 3) << endl;
+
+//     cout << (a =+ 10) << endl;
+//     cout << (a =- 5) << endl;
+    
+
+//     return 0;
+// }
+
