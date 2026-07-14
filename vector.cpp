e8e3cb5d074c95b1157e2a6572965c2b2b115514
navetiwari80-creator <navetiwari80@gmail.com>
2026-07-14 11:45:12 +0530

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+// int main(){
+//     vector<int> vec = {1, 3, 2, 5, 4};
+//     int min = vec[0];
+
+//     for(int i=0; i<=vec.size(); i++){
+//         if(vec[i] >  vec[i]){
+//             min = vec[i];
+//         }
+//     }
+//     cout << min;
+
+//     return 0;
+// }
+
+// int main(){
+//     vector<int> vec ={10,30,20,50,40};
+//     int max = vec[0];
+
+//     for(int i=0; i<vec.size(); i++){
+//         if(vec [i] > max){
+//             max = vec[i];
+//         }
+//     }
+//     cout << max << endl;
+//     return 0;
+// }
+
+// int main(){
+//     vector<string> vec(5, "Naveen");
+//     for(string s : vec){
+//         cout << s << endl;
+//     }
+//     return 0;
+// }
+
+
+// int main(){
+//     vector<int> vec = {1,3,2,5,4};
+//     for(int i=0; i<vec.size(); i++){
+//         if(vec[i] % 2 == 0){
+//         cout << vec[i] << " = is Even" << endl;
+//         }else{
+//         cout << vec[i] << " = is Odd" << endl;
+//         }
+//     }
+       
+//     return 0;
+// }
+
+
+
+
+// int main() {
+//     vector<int> vec = {10, 31, 20, 55, 40};
+
+//     int even = 0;
+//     int odd = 0;
+
+//     for(int i = 0; i < vec.size(); i++) {
+//         if(vec[i] % 2 == 0) {
+//             cout << vec[i] << " is Even" << endl;
+//             even++;
+//         } else {
+//             cout << vec[i] << " is Odd" << endl;
+//             odd++;
+//         }
+//     }
+
+//     cout << "\nEven is " << even << endl;
+//     cout << "Odd is " << odd << endl;
+
+//     return 0;
+// }
+
+
+// int main(){
+//     vector<char> ch = {'a','b','c','d','e'};
+    
+//     for(int i = 0; i < ch.size(); i++) {
+//         cout << ch[i] << " = " << (int)ch[i] << endl;
+//     }
+
+//     return 0;
+// }
+
+// int main(){
+//     vector<char> ch = {'A', 'B', 'C','D','E'};
+
+//     for(int i=0; i<ch.size(); i++){
+//         cout << ch[i] << " = " << (int)ch[i] << endl;
+//     }
+//     return 0;
+// }
+
+int main(){
+    vector<char> ch = {'N', 'A', 'V','E','E','N'};
+
+    for(int i=0; i<ch.size(); i++){
+        cout << ch[i] << " = " << (int)ch[i] << endl ;
+    }
+
+
+    return 0;
+}
