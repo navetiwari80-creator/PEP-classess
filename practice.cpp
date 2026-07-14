4a8d812f7240a9ddfba6f2c1229c630b7872f585
navetiwari80-creator <navetiwari80@gmail.com>
2026-07-14 11:51:34 +0530

    Add files via upload

--- practice.cpp
+++ practice.cpp
@@ -1,21 +1,111 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// using namespace std;
 
-int main(){
-    int nums[] = {2,7,11,15};
-    int target = 17;
+// int main(){
+//     int  n;
+//     cin >> n;
 
-    int n = sizeof(nums) / sizeof(nums[0]);
-    
-    for(int i = 0; i<n; i++){
-        for(int j = i+1; j<n; j++){
-            if((nums[i] + nums[j]) == target){
-                cout << "["<<i<<","<<j<<"]";
-                return 0;
-            }
-        }
-    }
+//     vector<int> vec;
 
-    cout << "No Pair Found" << endl;
-    return 0;
-}
+//     for(int i=0; i<n; i++){
+//         int num;
+//         cin >> num;
+//         vec.push_back(num);
+//     }
+
+//     for(int i=0; i<vec.size(); i++){
+//         cout << vec[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int main (){
+//     int n;
+//     cin >> n;
+
+//     vector<int> vec;
+//     for(int i=0; i<n; i++){
+//        int num;
+//        cout << num;
+//        vec.push_back(num);
+//     }
+
+//     for(int i=0; i<vec.size(); i++){
+//         cout << vec[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     string name = " ";
+
+//     for(int i=0; i<n; i++){
+//         char ch;
+//         cin >> ch;
+//         name += ch;
+//     }
+
+//     cout << name;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+//     string input;
+//     cin >> input;
+
+//     string name = "";
+
+//     for (char ch : input) {
+//         if (ch != ',') {
+//             name += ch;
+//         }
+//     }
+
+//     cout << name;
+
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int main(){
+//     vector<int> vec = {10,20,30,40,50};
+
+//     int i = 0;
+//     int j = vec.size() - 1;
+
+//     while(i<j){
+//         swap(vec[i], vec[j]);
+//         i++;
+//         j--;
+//     }
+
+//     for(int x : vec){
+//         cout << x << " ";
+//     }
+//     return 0;
+// }
+
+
