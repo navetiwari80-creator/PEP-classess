4b7cf52057c4dddc0dd76649f3221e6523f742cf
navetiwari80-creator <navetiwari80@gmail.com>
2026-07-20 00:00:09 +0530

    Add files via upload

---
+++ Address.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     int a = 5;
+//     cout<< &a << endl;
+//     return 0; 
+// }
+
+// int main(){
+//     int a = 10;
+//     int* ptr = &a;
+//     cout << ptr << endl;
+//     cout << &a << endl;
+    
+//     return 0;
+// }
+
+
+// int main(){
+//     float price = 100.25f;
+//     float* ptr = &price;
+
+//     cout << ptr << endl;
+//     cout << &price << endl;
+
+//     return 0;
+// }
+
+
+//
