748d594feed208f6a8bd1351871b201e91fb381d
Naveen Tiwari <navetiwari80@gmail.com>
2026-07-28 23:32:18 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,40 @@
+#print ("Naveen Tiwari")
+#print ("I am learning python ")
+
+# name = "Naveen Tiwari"
+# age = 21
+# price = 99.99
+
+# print(name)
+# print(age)
+# print(price)
+
+# #Identifiers 
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+
+# a = 23.23
+# old = a
+# print(type(old))
+
+# 1 program print two sum
+
+# a = 10 
+# b = 20
+# print(a+b)
+
+# a=12
+# b=32
+# sum = a+b
+# print(sum)
+
+a=12
+b=8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
