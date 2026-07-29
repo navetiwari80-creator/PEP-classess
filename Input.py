fb53199972639cc2030ae0305da8172dba40cdc0
Naveen Tiwari <navetiwari80@gmail.com>
2026-07-29 23:15:14 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,19 @@
+# 1. input()
+
+#input("Enter your Name : ")
+
+# 2.
+# name = input("Enter your name")
+# print("Welcome: ", name)
+
+# val = input("Enter some value :")
+# print(type(val), val)
+
+# 3. 
+
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+marks = float(input("Enter marks: "))
+print("Wlcome", name)
+print("Age =",  age)
+print("Marks :", marks)
