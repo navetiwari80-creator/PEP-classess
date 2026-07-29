b117b5b9dd31fba7b5987c7d6d7dab3d0e6cd751
Naveen Tiwari <navetiwari80@gmail.com>
2026-07-29 23:15:59 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,59 @@
+# a=12
+# b=8
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a%b)
+# print(a/b)
+
+# a=4
+# b=3
+
+# print(a ** b)
+
+#RELATIONAL OPERATOR
+
+# a = 5
+# b = 8
+# print (a == b)
+# print (a != b)
+# print (a <= b)
+# print (a >= b)
+# print (a > b)
+# print (a < b)
+
+#ASSIGNMENT OPERATOR
+
+# a = 4
+# print(a)
+
+# a += 5
+# print(a)
+
+# a += 5
+# print(a)
+# a -= 5
+# print(a)
+# a *= 5
+# print(a)
+# a /= 5
+# print(a)
+# a %= 5
+# print(a)
+# a ** a
+# print(a)
+
+#TYPE CONVERSION
+# a,b = 1,2.0
+# sum = a+b
+# print (sum)
+
+#TYPE CASTING
+# a,b = 1,"2"
+# c = int(b)
+# sum = a+c
+# print(sum)
+
+# a = 3.14
+# a = str(a)
+# print(type(a))
